e2a7abf983dbb7684f878b91b9cee010b3de8660
otang47 <78753719+otang47@users.noreply.github.com>
2021-09-10 01:13:00 -0400

    Add files via upload

--- portfolio/homepageScroll.js
+++ portfolio/homepageScroll.js
@@ -46,3 +46,105 @@ gsap.to(".name", {
   }
 })
 
+$(document).ready(function () {
+    var yOff = 15;
+    var xOff = -20;
+    var pathToImage = "assets/thumbnailChennaiSmall.png";
+
+    $(".text-hover-image").hover(function (e) {
+        $("body").append("<p id='image-when-hovering-text'><img src='" + pathToImage + "'/></p>");
+        $("#image-when-hovering-text")
+            .css("position", "absolute")
+            .css("width", "30%")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px")
+            .fadeIn("fast");
+    },
+
+    function () {
+        $("#image-when-hovering-text").remove();
+    });
+
+    $(".text-hover-image").mousemove(function (e) {
+        $("#image-when-hovering-text")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px");
+    });
+});
+
+$(document).ready(function () {
+    var yOff = 15;
+    var xOff = -20;
+    var pathToImage = "assets/yleanaThumbnailSmall.png";
+
+    $(".text-hover-image-two").hover(function (e) {
+        $("body").append("<p id='image-when-hovering-text'><img src='" + pathToImage + "'/></p>");
+        $("#image-when-hovering-text")
+            .css("position", "absolute")
+            .css("width", "30%")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px")
+            .fadeIn("fast");
+    },
+
+    function () {
+        $("#image-when-hovering-text").remove();
+    });
+
+    $(".text-hover-image-two").mousemove(function (e) {
+        $("#image-when-hovering-text")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px");
+    });
+});
+$(document).ready(function () {
+    var yOff = 15;
+    var xOff = -20;
+    var pathToImage = "assets/MockUpSlide2Small.png";
+
+    $(".text-hover-image-three").hover(function (e) {
+        $("body").append("<p id='image-when-hovering-text'><img src='" + pathToImage + "'/></p>");
+        $("#image-when-hovering-text")
+            .css("position", "absolute")
+            .css("width", "30%")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px")
+            .fadeIn("fast");
+    },
+
+    function () {
+        $("#image-when-hovering-text").remove();
+    });
+
+    $(".text-hover-image-three").mousemove(function (e) {
+        $("#image-when-hovering-text")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px");
+    });
+});
+
+$(document).ready(function () {
+    var yOff = 15;
+    var xOff = -20;
+    var pathToImage = "assets/communionGifSmall.gif";
+
+    $(".text-hover-image-four").hover(function (e) {
+        $("body").append("<p id='image-when-hovering-text'><img src='" + pathToImage + "'/></p>");
+        $("#image-when-hovering-text")
+            .css("position", "absolute")
+            .css("width", "30%")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px")
+            .fadeIn("fast");
+    },
+
+    function () {
+        $("#image-when-hovering-text").remove();
+    });
+
+    $(".text-hover-image-four").mousemove(function (e) {
+        $("#image-when-hovering-text")
+            .css("top", (e.pageY - yOff) + "px")
+            .css("left", (e.pageX + xOff) + "px");
+    });
+});
